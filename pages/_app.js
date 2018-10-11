9f8f056a705bc12995176265651e753556749b19
Simon Lydell <simon.lydell@gmail.com>
2018-10-11 09:40:06 +0200

    Fix error handling

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import ErrorComponent from './_error'
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
-    // Uncomment this and notice the difference compared to causing and error in
-    // pages/index.js. Here, the server just responds with “Internal Server
-    // Error”.
-    // await Promise.reject(new Error("_app.js error"))
-
     let pageProps = {}
 
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
+
+    // Do as little as possible when rendering the error page, in case something
+    // in this `getInitialProps` function fails. We don't want to error on the
+    // error page, so to speak.
+    if (Component === ErrorComponent) {
+      return { pageProps }
+    }
+
+    // Imagine doing an API request here: If that fails, we don't want that to
+    // fail the error page from rendering too. Besides, we probably don't want
+    // to make the API call on the error page in the first place.
+    await Promise.reject(new Error("_app.js error"))
+
 
     return { pageProps }
   }
--- pages/index.js
+++ pages/index.js
@@ -5,5 +5,5 @@ export default page;
 page.getInitialProps = async () => {
   // This renders the error overlay in development and the 500 page in
   // production as expected.
-  await Promise.reject(new Error("page error"));
+  // await Promise.reject(new Error("page error"));
 };
