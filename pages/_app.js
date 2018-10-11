5603c2b3ed61c8c889d0cd735d5630f76031e210
Simon Lydell <simon.lydell@gmail.com>
2018-10-11 09:55:27 +0200

    Even better solution if you need to call the API before child getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -6,22 +6,26 @@ export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {}
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
     // Do as little as possible when rendering the error page, in case something
     // in this `getInitialProps` function fails. We don't want to error on the
     // error page, so to speak.
     if (Component === ErrorComponent) {
+      if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx)
+      }
       return { pageProps }
     }
+
 
     // Imagine doing an API request here: If that fails, we don't want that to
     // fail the error page from rendering too. Besides, we probably don't want
     // to make the API call on the error page in the first place.
     await Promise.reject(new Error("_app.js error"))
 
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
 
     return { pageProps }
   }
